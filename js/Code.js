c064b9d89b8712d0dd6fb95d0547373476f051c8
Karan-sanjay-hande <82800630+Karan-sanjay-hande@users.noreply.github.com>
2021-06-01 14:24:16 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,21 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    text("U T F I N O C ", 700,160)
+    fill("lightblue")
+    text("Hint: makes main commands", 700,180)
+    
+    text("A A D T A S B E", 100,300)
+    fill("lightblue")
+    text("Hint: Used store data", 100,320)
+
+}
